46e6fc23837e6c84d6872d7c73074c785707d8d2
yoshi161 <yosua161@gmail.com>
2019-02-14 17:05:32 +0800

    Update index

--- src/index.js
+++ src/index.js
@@ -6,35 +6,43 @@ import rest from './rest';
 
 const $ = jquery;
 
-const detail = (e) => {
-    debugger
-    let id = $(e.target).attr('data-id');
-    let $detail = $(`.user-tr-detail-${id}`);
+//operation to display the post into the table
+const _populatePost = (posts, id) => {
+    posts.forEach(post=> {
+        $(`#user-tr-${id}:last`).after(`
+            <tr class="tb-white user-tr-detail-${id}">
+                <td colspan="6">
+                    <div><strong>${post.title}</strong></div>
+                    <div>${post.body}</div>
+                </td>
+            </tr>`);
+    });
+}
+
+//Show details when the button was clicked
+const _showDetail = (e) => {
+    let id = $(e.target).attr('data-id'),
+        $detail = $(`.user-tr-detail-${id}`);
+
     if($detail.length == 0) {
         rest.getPostById(id)
         .then((posts)=>{
-            posts.forEach(post=> {
-                $(`#user-tr-${id}:last`).after(`
-                    <tr class="tb-white user-tr-detail-${id}">
-                        <td colspan="6">
-                            <div><strong>${post.title}</strong></div>
-                            <div>${post.body}</div>
-                        </td>
-                    </tr>`);
-            });
+            _populatePost(posts, id);
         });
     } else {
         $(`.user-tr-detail-${id}`).remove();
     }
 }
 
-const addDetailEvent = (user) => {
+//add click event into the button
+const _addDetailEvent = (user) => {
     $(`#detail-button-${user.id}`).click((e)=> {
-        detail(e);
+        _showDetail(e);
     });
 }
 
-const populateUser = (user, index) => {
+//operation to display the user into the table
+const _populateUser = (user, index) => {
     $('#users-table>tbody')
     .append(`<tr id="user-tr-${user.id}">
                 <th>${index+1}</th>
@@ -48,10 +56,11 @@ const populateUser = (user, index) => {
             </tr>`);
 }
 
+//operation to retrieve the users
 rest.getUsers()
 .then((users)=>{
     users.forEach((user, i) => {
-        populateUser(user, i);
-        addDetailEvent(user);
+        _populateUser(user, i);
+        _addDetailEvent(user);
     })
 });
